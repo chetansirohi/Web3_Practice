bfb688f46c9598293e612eebdd6c58c7a7234742
chetansirohi <35464633+chetansirohi@users.noreply.github.com>
2022-10-18 14:54:43 +0530

    adding SampleNFT

---
+++ SimpleNFT/scripts/deploy.js
@@ -0,0 +1,31 @@
+// Import ethers from Hardhat package
+const { ethers } = require("hardhat");
+
+async function main() {
+  /*
+A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+so nftContract here is a factory for instances of our GameItem contract.
+*/
+  const nftContract = await ethers.getContractFactory("GameItem");
+
+  // here we deploy the contract
+  const deployedNFTContract = await nftContract.deploy();
+
+  // wait for the contract to deploy
+  await deployedNFTContract.deployed();
+
+  // print the address of the deployed contract
+  console.log("NFT Contract Address:", deployedNFTContract.address);
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+
+
+// NFT Contract Address: 0xba6474BB033cdAE0a5dF5ec7EB7DcDBEeA294D88
