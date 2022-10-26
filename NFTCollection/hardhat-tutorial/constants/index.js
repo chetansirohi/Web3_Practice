68dce83dc5daa576cede1b11a8b40f6b309a5199
chetansirohi <35464633+chetansirohi@users.noreply.github.com>
2022-10-26 17:45:39 +0530

    Delete NFTCollection directory
    Rework the project

--- NFTCollection/hardhat-tutorial/constants/index.js
+++
@@ -1,6 +0,0 @@
-// Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0x319749d223849b6ce390F6020AD90921Be92a3f9";
-// URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-psi-weld.vercel.app/api/";
-
-module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
