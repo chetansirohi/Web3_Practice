4258aa6a2a911924756fc69086602d0a4c6206dd
chetansirohi <35464633+chetansirohi@users.noreply.github.com>
2022-10-27 22:37:58 +0530

    Delete ICO directory

--- ICO/hardhat-tutorial/constants/index.js
+++
@@ -1,4 +0,0 @@
-// Address of the NFT Contract that you deployed
-const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xb3DDbe9ACaA568aa6d2E0a5A6548A66af7d565B3";
-
-module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
