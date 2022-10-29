e4b4026bd53e2168f3a32a6b7190da1013182a05
chetansirohi <35464633+chetansirohi@users.noreply.github.com>
2022-10-29 12:36:51 +0530

    added refactored ICO code

---
+++ ICODapp/hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x7aC0dFE9feAd1f09cAEcc1eb5d7B52069A4b2635";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
