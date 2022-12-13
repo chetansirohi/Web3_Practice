cc7c9a5425f3b9c222031efa00977a7d6bb0f3e4
chetansirohi <35464633+chetansirohi@users.noreply.github.com>
2022-12-13 13:48:25 +0530

    solidity Remote Purchase Example

---
+++ SolcRemotePurchase/Purchase.sol
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.4;
+
+contract Purchase {
+    uint256 public value;
+    address payable public seller;
+    address payable public buyer;
+
+    enum State {
+        Created,
+        Locked,
+        Release,
+        Inactive
+    }
+    State public state;
+
+    modifier condition(bool condition_) {
+        require(condition_);
+        _;
+    }
+
+    error OnlyBuyer();
+    error OnlySeller();
+    error InvalidState();
+    error ValueNotEven();
+
+    modifier onlyBuyer() {
+        if (msg.sender != buyer) revert OnlyBuyer();
+        _;
+    }
+
+    modifier onlySender() {
+        if (msg.sender != seller) revert OnlyBuyer();
+        _;
+    }
+
+    modifier inState(State state_) {
+        if (state != state_) revert InvalidState();
+        _;
+    }
+
+    event Aborted();
+    event PurchaseConfirmed();
+    event ItemReceived();
+    event SellerRefunded();
+
+    constructor() payable{
+        seller =payable(msg.sender);
+        value = msg.value/2;
+        if((2*value) !=msg.value)
+        revert ValueNotEven();
+    }
+
+    function abort() external inState(State.Created){
+        emit Aborted();
+        state = State.Inactive;
+        seller.transfer(address(this).balance);
+    }
+
+    function confirmPurchase() external inState(State.Created) condition(msg.value == (2*value)) payable{
+        emit PurchaseConfirmed();
+        buyer = payable(msg.sender)
+    }
+
+    function confirmReceived() external onlyBuyer inState(State.Locked) {
+        emit ItemReceived();
+        state= State.Release;
+        buyer.transfer(value);
+
+    }
+
+    function refundSeller() external onlySeller inState(State.Release) {
+        emit SellerRefunded();
+        state = State.Inactive;
+        seller.transfer(3*value);
+    }
+}
